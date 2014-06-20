2811285974673a784cf852708cc14177ea0994dd
Ryan Moore <moorer@udel.edu>
2014-06-20 14:12:57 -0400

    Update copyright

--- project.clj
+++ project.clj
@@ -1,6 +1,24 @@
+;; Copyright 2014 Ryan Moore
+
+;; This file is part of recruitment_info.
+
+;; recruitment_info is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; recruitment_info is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with recruitment_info.  If not, see
+;; <http://www.gnu.org/licenses/>.
+
 (defproject recruitment_info "0.0.1"
   :description "Recruitment info from BAM files."
-  :url ""
+  :url "https://github.com/mooreryan/recruitment_info"
   :license {:name "GNU General Public License, GPLv3"
             :url "http://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
--- spec/recruitment_info/alignment_info_spec.clj
+++ spec/recruitment_info/alignment_info_spec.clj
@@ -1,3 +1,21 @@
+;; Copyright 2014 Ryan Moore
+
+;; This file is part of recruitment_info.
+
+;; recruitment_info is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; recruitment_info is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with recruitment_info.  If not, see
+;; <http://www.gnu.org/licenses/>.
+
 (ns recruitment_info.alignment-info-spec
   (:require [speclj.core :refer :all]
             [recruitment_info.alignment-info :refer :all]))
