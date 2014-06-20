5a37532622b1d4c586890ba48f65a7cd566755f6
Ryan Moore <moorer@udel.edu>
2014-06-20 13:26:22 -0400

    Remove unnecessary files

--- project.clj
+++ project.clj
@@ -4,11 +4,8 @@
   :license {:name "GNU General Public License, GPLv3"
             :url "http://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [me.raynes/fs "1.4.4"]
                  [htsjdk/samtools "1.114"]
                  [picard "1.114"]
-                 [incanter "1.5.5"]
-                 [org.apache.commons/commons-math3 "3.3"]
                  [org.clojure/tools.cli "0.3.1"]]
   :repositories [["local" 
                   {:url ~(str (.toURI (java.io.File. "repo")))}]]
